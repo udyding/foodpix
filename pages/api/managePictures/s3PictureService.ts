f58dbc577a661e005e1b27ddca05d7008e151203
Udy Ding <udy.ding@gmail.com>
2021-09-21 19:41:20 -0400

    change aws env variables

--- pages/api/managePictures/s3PictureService.ts
+++ pages/api/managePictures/s3PictureService.ts
@@ -2,9 +2,9 @@ import S3 from 'aws-sdk/clients/s3'
 import { ReadStream } from 'fs'
 
 const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_BUCKET_REGION,
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: process.env.AWS_S3_BUCKET_REGION,
 })
 
 export const uploadPictureToS3 = async (
@@ -14,7 +14,7 @@ export const uploadPictureToS3 = async (
   try {
     const params = {
       Body: fileContent,
-      Bucket: process.env.AWS_BUCKET_NAME,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: fileName,
     }
     return s3.upload(params).promise()
@@ -26,7 +26,7 @@ export const uploadPictureToS3 = async (
 export const getPresignedUrl = async (fileKey: string): Promise<string> => {
   const params = {
     Key: fileKey,
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
     Expires: 100, // seconds
   }
   return s3.getSignedUrlPromise('getObject', params)
